4e5dd4fcc81771048d5cbe09c0b24f1aa27f87ff
kalbetre-alx <kalbetre@icloud.com>
2022-07-11 23:53:00 +0000

    update logic of printf

--- _printf.c
+++ _printf.c
@@ -2,6 +2,23 @@
 #include <unistd.h>
 
 /**
+ * check_buffer_overflow - if writing over buffer space,
+ * print everything then revert length back to 0 to write at buffer start
+ * @buffer: buffer holding string to print
+ * @len: position in buffer
+ * Return: length position
+ */
+int check_buffer_overflow(char *buffer, int len)
+{
+	if (len > 1020)
+	{
+		write(1, buffer, len);
+		len = 0;
+	}
+	return (len);
+}
+
+/**
  * _printf - prints a string
  * @format: the string to print
  *
@@ -9,44 +26,82 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ls;
-	int len = 0, buf_len = 0;
-	char *(*print_func)(va_list);
-	char *buffer, *temp_str;
+	int len = 0, total_len = 0, i = 0, j = 0;
+	va_list list;
+	char *buffer, *str;
+	char* (*f)(va_list);
 
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-		return (-1);
 	if (format == NULL)
 		return (-1);
-	if ((*format == '%') && (*(format + 1) == '\0' || *(format + 1) == ' '))
+
+	buffer = create_buffer();
+	if (buffer == NULL)
 		return (-1);
 
-	va_start(ls, format);
-	while (*format != '\0')
+	va_start(list, format);
+
+	while (format[i] != '\0')
 	{
-		if (*format == '%')
+		if (format[i] != '%') /* copy format into buffer until '%' */
 		{
-			format++;
-			print_func = get_print_func(*format);
-			if (print_func == NULL)
+			len = check_buffer_overflow(buffer, len);
+			buffer[len++] = format[i++];
+			total_len++;
+		}
+		else /* if %, find function */
+		{
+			i++;
+			if (format[i] == '\0') /* handle single ending % */
 			{
-				buf_len = add_char_to_buffer(buffer, buf_len, &len, '%');
-				buf_len = add_char_to_buffer(buffer, buf_len, &len, *format);
+				va_end(list);
+				free(buffer);
+				return (-1);
+			}
+			if (format[i] == '%') /* handle double %'s */
+			{
+				len = check_buffer_overflow(buffer, len);
+				buffer[len++] = format[i];
+				total_len++;
 			}
 			else
 			{
-				temp_str = print_func(ls);
-				buf_len = add_str_to_buffer(buffer, buf_len, &len, temp_str);
-				free(temp_str);
-			}
+				f = get_print_func(format[i]); /* grab function */
+				if (f == NULL)  /* handle fake id */
+				{
+					len = check_buffer_overflow(buffer, len);
+					buffer[len++] = '%';
+					total_len++;
+					buffer[len++] = format[i];
+					total_len++;
+				}
+				else /* return string, copy to buffer */
+				{
+					str = f(list);
+					if (str == NULL)
+					{
+						va_end(list);
+						free(buffer);
+						return (-1);
+					}
+					if (format[i] == 'c' && str[0] == '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = '\0';
+						total_len++;
+					}
+					j = 0;
+					while (str[j] != '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = str[j];
+						total_len++;
+						j++;
+					}
+					free(str);
+				}
+			} i++;
 		}
-		else
-		{
-			buf_len = add_char_to_buffer(buffer, buf_len, &len, *format);
-		}
-		format++;
 	}
-	close_buffer(buffer, buf_len, &len, ls);
-	return (len - 1);
+	write_buffer(buffer, len, list);
+	return (total_len);
 }
--- buffer_helpers.c
+++ buffer_helpers.c
@@ -2,6 +2,37 @@
 #include <unistd.h>
 
 /**
+ * write_buffer - prints buffer, then frees it and frees va_list
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+void write_buffer(char *buffer, int len, va_list list)
+{
+	char *buff;
+
+	buff = realloc(buffer, len); /* realloc to correct size */
+	write(1, buff, len);		 /* print */
+
+	free(buff);
+	va_end(list);
+}
+
+/**
+ * create_buffer - creates buffer to hold string until it's ready for print
+ * Return: pointer to buffer created
+ */
+char *create_buffer(void)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (NULL);
+	return (buffer);
+}
+
+/**
  * add_char_to_buffer - adds a character to a buffer
  * @buffer: the buffer to add to
  * @buf_len: the length of the buffer
